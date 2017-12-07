d9498e72b965e204f06b777dde265831484897c2
Anjali Bansal <anjali.mca17.du@gmail.com>
2017-12-07 22:55:38 +0530

    Update ProbabilityAnalysisModules.R

--- ProbabilityAnalysisModules.R
+++ ProbabilityAnalysisModules.R
@@ -9,6 +9,7 @@ PERMUTATION <- function(n,r){
   return(nPr)
 }
 
+
 COMBINATION <- function(n,r){
   #Assumptions  : Works well with valid inputs.
   #Valid Inputs : n can take positive value greater than or equal to 1.
@@ -18,4 +19,27 @@ COMBINATION <- function(n,r){
   denominator <- FACTORIAL(r)*FACTORIAL(n-r)
   nCr <- numerator/denominator
   return(nCr)
+}
+
+
+BASICPROBABILITY <- function(favorable,total){
+  #Assumptions : Works well for valid input i.e, favorable <= total and both positive.
+  probability <- favorable/total
+  return(probability)
+}
+
+
+BAYESTHEOREM <- function(pofAi, pofBAi, i) {
+  len <- length(pofAi)
+  numerator <- as.numeric(pofAi[i])*as.numeric(pofBAi[i])
+  denominator <- 0
+  
+  for(j in 1:len){
+    product <- as.numeric(pofAi[j])*as.numeric(pofBAi[j])
+    denominator <- denominator + product
+  }
+  
+  result <- (numerator/denominator)
+  pofAiB <- formatC(result, digits = 6, format = "f")
+  return(pofAiB)
 }
